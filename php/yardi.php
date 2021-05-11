18a28b1e9f971fdea55f232c0015c739ff6e50ee
Howard Rosita <howard@thecamel.net>
2021-05-11 13:54:31 +0800

    Checking Php

--- php/yardi.php
+++ php/yardi.php
@@ -3,37 +3,52 @@
 function getTimeSlot($data){
 
   $curl = curl_init();
-  
+
   curl_setopt_array($curl, array(
-    CURLOPT_URL => ''.APIEndpoint.'AvailableSlots?MarketingAPIKey='.APIKey.'&CompanyCode=c00000110537&PropertyId='.$data->propertyID,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS =>'',
-    CURLOPT_HTTPHEADER => array(
+  CURLOPT_URL => ''.PIEndpoint.'AvailableSlots?MarketingAPIKey='.APIKey.'&CompanyCode=c00000110537&PropertyId='.$data->propertyID,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'',
+  CURLOPT_HTTPHEADER => array(
       'Cookie: __cfduid=d71a6227343b31c935d8c1068dcc13ec21619440578'
-    ),
+  ),
   ));
-  
+
   $resp = curl_exec($curl);
 
-  //Check Response Code eg: 240 / 200 / 400 etc.
-	$responseCode = curl_getinfo($curl,CURLINFO_HTTP_CODE);
-  
-  curl_close($curl);
+  // //Check Response Code eg: 240 / 200 / 400 etc.
+  // $responseCode = curl_getinfo($curl,CURLINFO_HTTP_CODE);
 
   //json decode
   $response = json_decode($resp);
 
-  if($response->ErrorCode == 0){
-      return ["status"=>true,"response"=>$response,"request"=>__FUNCTION__];
-  }else{
-      return ["status"=>false,"response"=>$response->ErrorMessage,"request"=>__FUNCTION__];
+  $status = true;
+  if($response->ErrorCode !== 0){
+      $response = $response->ErrorMessage;
+      $status = false;
   }
+
+  if(curl_error($curl)){
+      return ["status"=>$status,"response"=>"".curl_error($curl)."","request"=>__FUNCTION__];
+  }else{
+      return ["status"=>$status,"response"=>$response,"request"=>__FUNCTION__];
+  }
+
+  curl_close($curl);
+
+  //json decode
+  //$response = json_decode($resp);
+
+  // if($response->ErrorCode == 0){
+  //     return ["status"=>true,"response"=>$response,"request"=>__FUNCTION__];
+  // }else{
+  //     return ["status"=>false,"response"=>$response->ErrorMessage,"request"=>__FUNCTION__];
+  // }
 
 }
 
